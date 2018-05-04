4e48d77f5f26f055e5baf80f7a495155d3e29bfc
Omar Anser <anseromar@outlook.fr>
2018-05-04 08:55:24 +0200

    label

--- src/main/org/insa/algo/shortestpath/BellmanFordAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/BellmanFordAlgorithm.java
@@ -27,7 +27,7 @@ public class BellmanFordAlgorithm extends ShortestPathAlgorithm {
 
 		// Initialize array of distances.
 		double[] distances = new double[nbNodes];
-		//Mettre toutes les distances a INFINI
+		//Mettre toutes les distances à INFINI
 		Arrays.fill(distances, Double.POSITIVE_INFINITY);
 		distances[data.getOrigin().getId()] = 0;
 
